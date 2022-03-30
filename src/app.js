860a3ea5e3ec78003890ee6723bc6755923d4b6b
Sergio Bosch <sergio.bosch@gmail.com>
2022-03-30 18:35:14 +0200

    Added JS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 
-
-
 const name = 'world';
 console.log(`Hello ${name}`);
-
+if (!!document.getElementById('disco')) {
+    document.getElementById("disco").onclick = function () {
+        console.log("Carátula del disco");
+    }
+}
