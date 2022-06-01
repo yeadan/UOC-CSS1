b35f0dc14788b4a01b3a8e2a96f5e9c5cc849120
Sergio Bosch <sergio.bosch@gmail.com>
2022-06-01 18:10:54 +0200

    lazyload

--- src/app.js
+++ src/app.js
@@ -6,3 +6,26 @@ if (!!document.getElementById('disco')) {
         console.log("Carátula del disco");
     }
 }
+
+// Añadimos a app.js el lazy loading
+
+if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll('img[loading="lazy"]');
+    images.forEach(img => {
+        img.src = img.dataset.src;
+        img.removeAttribute('data-src');
+        img.srcset = img.dataset.srcset;
+        img.removeAttribute('data-srcset');
+    });
+    sources.forEach((source) => {
+        source.srcset = source.dataset.srcset;
+        source.removeAttribute('data-srcset');
+    });
+} else {
+    // Dynamically import the LazySizes library
+    const script = document.createElement('script');
+    script.src =
+        'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+    document.body.appendChild(script);
+}
+
